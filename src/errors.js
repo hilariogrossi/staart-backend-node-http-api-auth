d91e3ad53fdf70530559de80ab26b7b0d19f0f06
Rodrigo Gianotto <rodrigo.gianotto@timeprimo.com>
2022-10-03 15:17:12 -0300

    initial-commit

---
+++ src/errors.js
@@ -0,0 +1,30 @@
+class DomainError extends Error {
+  constructor(message) {
+    super(message)
+    this.name = this.constructor.name
+  }
+}
+
+class NotFoundError extends DomainError {
+  constructor({ resourceName, resourceId }) {
+    super(`Resource '${resourceName}' with identifier '${resourceId}' not found`)
+    this.resourceName = resourceName
+    this.resourceId = resourceId
+  }
+}
+
+class ValidationError extends DomainError {
+  constructor({ message = 'Invalid parameters', validations }) {
+    super(message)
+    this.validations = validations
+  }
+}
+
+class ConflictError extends DomainError {
+}
+
+module.exports = {
+  NotFoundError,
+  ValidationError,
+  ConflictError,
+}
